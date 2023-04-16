a2b40e75dc8f9cee3ab43cf770ee225e58d1dfc5
matheusrfferreira <mrff@tuta.io>
2023-04-16 19:24:45 -0300

    update: new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
-import Billboard from "@/netflix-next-web/components/Billboard";
-import Navbar from "@/netflix-next-web/components/Navbar";
-import useCurrentUser from "@/netflix-next-web/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+
+import Billboard from "../components/Billboard";
+import Navbar from "../components/Navbar";
+import useCurrentUser from "../hooks/useCurrentUser";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,11 +26,19 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div
+        className="
+          pb-40
+        "
+      >
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   )
 }
